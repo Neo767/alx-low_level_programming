32a2bf5b28bdad2a9b49f8121dcdf298769b1d20
Neo767 <matoane.ss@gmail.com>
2022-12-10 01:29:59 -0800

    time to puts c to the test - comment fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /*
- * main - input
+ * main: displaying output
  * return: returning 0
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+
+	return (0);
 }

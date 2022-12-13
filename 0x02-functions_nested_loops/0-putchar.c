c4a54aca5956a13ddee389afe5420b2fd962b059
Neo767 <matoane.ss@gmail.com>
2022-12-13 11:55:07 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ * auth: Neo767
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	_puthcar('_');
+	_puthcar('p');
+	_puthcar('u');
+	_puthcar('t');
+	_puthcar('t');
+	_puthcar('c');
+	_puthcar('h');
+	_puthcar('a');
+	_puthcar('r');
+	_puthcar('\n');
+	return (0);
+}
+

1273af318c105bbddbd3c7c1707e6b2418b78413
Neo767 <matoane.ss@gmail.com>
2022-12-13 15:25:04 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase
+ * auth: Neo767
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+
+	_putchar('\n');
+}

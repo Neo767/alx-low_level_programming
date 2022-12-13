0d791ed621ef0e56a0dc238d316eb2734f84018f
Neo767 <matoane.ss@gmail.com>
2022-12-13 15:53:06 -0800

    betty fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10: print lowercase alphabet ten times
+ * print_alphabet_x10: prints 10 times the alphabet, in lowercase
  * auth: Neo767
  */
 
 void print_alphabet_x10(void)
-{	
-	int count = 0;
+{
+	int cnt = 0;
 	char l;
-	while (count++ <= 9)
+
+	while (cnt++ <= 9)
 	{
 		for (l = 'a'; l <= 'z'; l++)
 			_putchar(l);
-		_putchar('\n')
+		_putchar('\n');
 	}
 }

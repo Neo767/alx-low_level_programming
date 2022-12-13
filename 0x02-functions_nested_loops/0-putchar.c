2a517ae1e056ee5bc0fba4c83f61b802e5bebef9
Neo767 <matoane.ss@gmail.com>
2022-12-13 12:35:20 -0800

    uhm

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,22 +3,19 @@
 /**
  * main - print _putchar
  * auth: Neo767
- * Return: Always 0 (Success)
- *
+ * Return: 0
  */
 
 int main(void)
 {
-	_puthcar('_');
-	_puthcar('p');
-	_puthcar('u');
-	_puthcar('t');
-	_puthcar('t');
-	_puthcar('c');
-	_puthcar('h');
-	_puthcar('a');
-	_puthcar('r');
-	_puthcar('\n');
-	return (0);
+_puthcar('_');
+_puthcar('p');
+_puthcar('u');
+_puthcar('t');
+_puthcar('c');
+_puthcar('h');
+_puthcar('a');
+_puthcar('r');
+_puthcar('\n');
+return (0);
 }
-

fd632867394728e60eb09b2fd606e48f4ca29e52
Neo767 <matoane.ss@gmail.com>
2022-12-14 07:20:39 -0800

    send up

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: integer to evaluate
+ * auth: Neo767
+ * Return: abs value of n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n)
+}
